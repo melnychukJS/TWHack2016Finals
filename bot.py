c6eb43409a3bf9995f5e985b826b0cb4d21d9cbe
Javier <javierhonduco@gmail.com>
2016-04-23 16:30:55 +0300

    minimal api thingie working

--- bot.py
+++ bot.py
@@ -4,6 +4,7 @@ import re
 
 from telegram import InlineQueryResultArticle, ParseMode
 from telegram.ext import Updater
+import requests
 import logging
 
 # Enable logging
@@ -13,19 +14,32 @@ logging.basicConfig(
 
 logger = logging.getLogger(__name__)
 
+REGEX = '\$|€|£|pound[s]|euro?s?|dollar[s]'
 
 WELCOME_MESSAGE = """Hi!
-I'm transferbot and I'm here to help you.
-You can call me anywhere and issue a transfer :)
+I'm transferbot and I can help you sending money :)
 
-/pay to issue a payment.
-/quote to check the quote offer.
+Example of useful commands:
+
+- Money transferring & exchange:
+    /pay 50€ [to dollars] to @username
+
+- Check the fee that you would be charged:
+    /fee $20
+
+Powered by TransferWise™. 
+(Disclaimer: This product is not created neither endorsed with TransferWise™ in any ways.)
+"""
 
 
-With /help you can see more info."""
+HELP_MESSAGE = """
+Example of useful commands:
 
+- Money transferring & exchange:
+    /pay 50€ [to dollars] to @username
 
-HELP_MESSAGE = """Help
+- Check the fee that you would be charged:
+    /fee $20
 """
 
 PAY_MESSAGE = """Cool!
@@ -48,14 +62,76 @@ def help(bot, update):
 def pay(bot, update):
     text = update['message']['text']
 
-    money = re.findall('[0-9]+', text)[0]
-    currency = re.findall('\$|€', text)[0]
-    to = re.findall('@[a-zA-Z]+', text)[0]
-    bot.sendMessage(update.message.chat_id, text=PAY_MESSAGE.format(money, currency, to))
+    money = re.findall('[0-9]+', text)
+    currency = re.findall(REGEX, text)
+
+    to = re.findall('@[a-zA-Z]+', text)
+    if len(money) == 0:
+        bot.sendMessage(update.message.chat_id, text="Please, input the amount. See /help for tips.")
+    elif len(currency) == 0:
+        bot.sendMessage(update.message.chat_id, text="Please, input the currency. See /help for tips.")
+    elif(len(to)==0):
+        bot.sendMessage(update.message.chat_id, text="Please, input the receiver. See /help for tips.")
+    else:
+        if len(currency)==1:
+            bot.sendMessage(update.message.chat_id, text=PAY_MESSAGE.format(money[0], currency[0], to[0]))
+        else:
+            bot.sendMessage(update.message.chat_id, text="""Cool!
+    {}{} converted to {} and sent to {}!
+    """.format(money[0], currency[0], currency[1], to[0]))
+
+def to_iso(input):
+    return {
+        'eur': 'EUR',
+        'euro': 'EUR',
+        'euros': 'EUR',
+        '€': 'EUR',
+        'pound': 'GBP',
+        'pounds': 'GBP',
+        '£': 'GBP',
+        'dollar': 'USD',
+        'dollars': 'USD',
+        '$': 'USD',
+    }.get(input, 'EUR')
 
 
 def fee(bot, update):
-    bot.sendMessage(update.message.chat_id, text=FEE_MESSAGE.format(0.5, '€'))
+    text = update['message']['text']
+
+    money = re.findall('[0-9]+', text)
+    currency = re.findall(REGEX, text)
+
+    if len(money)==0:
+        bot.sendMessage(update.message.chat_id, text="Please, input the amount.")
+    elif len(currency)==0:
+        bot.sendMessage(update.message.chat_id, text="Please, input the currency.")
+    else:
+
+        currency = re.findall(REGEX, text)
+
+    if len(money)==0:
+        bot.sendMessage(update.message.chat_id, text="Please, input the amount.")
+    elif len(currency)==0:
+        bot.sendMessage(update.message.chat_id, text="Please, input the currency.")
+    else:
+        headers = {'Content-type': 'application/json'}
+
+        currency = re.findall(REGEX, text)
+        if len(currency)>1:
+
+            response = requests.get('https://test-restgw.transferwise.com/v1/quotes?source={}&target={}&sourceAmount={}&rateType=FIXED'.format(
+                to_iso(currency[0]),
+                to_iso(currency[1]),
+                money[0]
+            ), 
+            headers=headers,
+            auth=('b909eac5-b567-4ca2-a55d-4cb2eeb74a79', '5cfe25e0-f322-404a-b0d3-537d262b2fd0')).json()
+
+            print(response)
+            bot.sendMessage(update.message.chat_id, text=FEE_MESSAGE.format(response['fee'], currency[0]))
+        else:
+            bot.sendMessage(update.message.chat_id, text=FEE_MESSAGE.format("shit", currency[0]))
+
 
 
 def escape_markdown(text):
